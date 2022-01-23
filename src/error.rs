f44e1fc27ac2fcb7a55f9984046f9f97d77725a5
saiintbrisson <luizcmpc@gmail.com>
2022-01-22 23:38:34 -0300

    feat: added erro variants

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,31 @@
-// pub struct ParseError;
-// pub struct Req;
+use std::str::Utf8Error;
+
+#[derive(Debug, thiserror::Error)]
+pub enum SessionError {
+    #[error("failed to handle request {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("invalid request {0}")]
+    InvalidRequest(http::Error),
+    #[error("invalid response {0}")]
+    InvalidResponse(http::Error),
+}
 
 #[derive(Debug, thiserror::Error)]
 pub enum RequestError {
     #[error("failed to handle request {0}")]
     IoError(#[from] std::io::Error),
-    #[error("invalid method {0}")]
-    InvalidMethod(String),
-    #[error("path {0} not found")]
-    NotFound(String),
+    #[error("invalid encoding {0}")]
+    InvalidEncoding(#[from] Utf8Error),
+    #[error("invalid format")]
+    InvalidFormat,
+    #[error("http error {0}")]
+    HttpError(#[from] http::Error),
+    #[error("unsupported version")]
+    UnsupportedVersion,
 }
+
+// impl From<ToStrError> for RequestError {
+//     fn from(_: ToStrError) -> Self {
+//         Self::InvalidEncoding
+//     }
+// }
